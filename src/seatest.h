0fafcc0d68f2dd190622b9a831d11aefbbbf0ebf
Terry Moore <tmm@mcci.com>
2018-10-22 17:52:21 -0400

    More fix #1: add the macro (had the routine)

--- src/seatest.h
+++ src/seatest.h
@@ -66,6 +66,7 @@ Assert Macros
 #define assert_false(test) do {  seatest_assert_false(test, __FUNCTION__, __LINE__); } while (0)
 #define assert_int_equal(expected, actual) do {  seatest_assert_int_equal(expected, actual, __FUNCTION__, __LINE__); } while (0)
 #define assert_ulong_equal(expected, actual) do {  seatest_assert_ulong_equal(expected, actual, __FUNCTION__, __LINE__); } while (0)
+#define assert_size_t_equal(expected, actual) do {  seatest_assert_size_t_equal(expected, actual, __FUNCTION__, __LINE__); } while (0)
 #define assert_string_equal(expected, actual) do {  seatest_assert_string_equal(expected, actual, __FUNCTION__, __LINE__); } while (0)
 #define assert_n_array_equal(expected, actual, n) do { size_t seatest_count; for(seatest_count=0; seatest_count<n; seatest_count++) { char s_seatest[SEATEST_PRINT_BUFFER_SIZE]; sprintf(s_seatest,"Expected %d to be %d at position %d", actual[seatest_count], expected[seatest_count], (int)seatest_count); seatest_simple_test_result((expected[seatest_count] == actual[seatest_count]), s_seatest, __FUNCTION__, __LINE__);} } while (0)
 #define assert_bit_set(bit_number, value) { seatest_simple_test_result(((1 << bit_number) & value), " Expected bit to be set" ,  __FUNCTION__, __LINE__); } while (0)
