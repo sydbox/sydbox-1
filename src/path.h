a0091574aa136c20d6ec5e407dc2ecc198137c59
Alï Polatel <alip@exherbo.org>
2021-05-24 14:52:26 +0200

    path_is_absolute: return bool rather than int
    Signed-off-by: Alï Polatel <alip@exherbo.org>

--- src/path.h
+++ src/path.h
@@ -3,7 +3,7 @@
  *
  * Path related utilities
  *
- * Copyright (c) 2012 Ali Polatel <alip@exherbo.org>
+ * Copyright (c) 2012, 2021 Ali Polatel <alip@exherbo.org>
  * Based in part upon systemd which is:
  *   Copyright (C) 2010-2012 Lennart Poettering
  * Distributed under the terms of the GNU Lesser General Public License v2.1 or later
@@ -15,7 +15,7 @@
 extern char *path_make_absolute(const char *p, const char *prefix);
 extern char *path_kill_slashes(char *path);
 
-static inline int path_is_absolute(const char *p)
+static inline bool path_is_absolute(const char *p)
 {
 	return p[0] == '/';
 }
