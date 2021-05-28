e268c3a8f6bd65c212baa2b586b4ca4457c9dbf4
Alï Polatel <alip@exherbo.org>
2021-05-28 09:50:23 +0200

    pandora: initial replacement for shoebox in rust
    Signed-off-by: Alï Polatel <alip@exherbo.org>

---
+++ build.rs
@@ -0,0 +1,11 @@
+//
+// pandora: Sydbox's Dump Inspector & Profile Writer
+// build.rs: Helper file for build-time information
+//
+// Copyright (c) 2021 Ali Polatel <alip@exherbo.org>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+fn main() {
+    built::write_built_file().expect("Failed to acquire build-time information");
+}
