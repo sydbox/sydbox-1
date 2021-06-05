f41bf9d911f919ea293b3bc0652068f7c1cb6baa
Alï Polatel <alip@exherbo.org>
2021-06-05 19:23:11 +0200

    misc: update prep-env for clang-{10.0->11}
    Signed-off-by: Alï Polatel <alip@exherbo.org>

--- misc/prep-env-clang.sh
+++ misc/prep-env-clang.sh
@@ -1,6 +1,12 @@
-#!/bin/sh -x
+#!/bin/bash -x
 
 CC="$(which clang)"
+if "${CC}" --version | grep -q 10.0; then
+    CC_11="$(which clang-11)"
+    if [ -n "$CC_11" ]; then
+        CC="$CC_11"
+    fi
+fi
 if [[ ! -x "$CC" ]]; then
     echo >&2 "clang not found in PATH"
     exit 1
