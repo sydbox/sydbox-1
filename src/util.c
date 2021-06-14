77a5cd397c545e8926b631c175fb49932066cd6b
Alï Polatel <alip@exherbo.org>
2021-06-14 05:09:27 +0200

    send_fd: drop unused function
    Signed-off-by: Alï Polatel <alip@exherbo.org>

--- src/util.c
+++ src/util.c
@@ -290,35 +290,6 @@ ssize_t atomic_write(int fd, const void *buf, size_t count)
 	return total;
 }
 
-int send_fd(int sock, int fd)
-{
-	struct msghdr msg;
-	struct cmsghdr *cmsg;
-	char buf[CMSG_SPACE(sizeof(int))] = {0}, c = 'c';
-	struct iovec io = {
-		.iov_base = &c,
-		.iov_len = 1,
-	};
-
-	msg.msg_iov = &io;
-	msg.msg_iovlen = 1;
-	msg.msg_control = buf;
-	msg.msg_controllen = sizeof(buf);
-	cmsg = CMSG_FIRSTHDR(&msg);
-	cmsg->cmsg_level = SOL_SOCKET;
-	cmsg->cmsg_type = SCM_RIGHTS;
-	cmsg->cmsg_len = CMSG_LEN(sizeof(int));
-	*((int *)CMSG_DATA(cmsg)) = fd;
-	msg.msg_controllen = cmsg->cmsg_len;
-
-	if (sendmsg(sock, &msg, 0) < 0) {
-		perror("sydbox: sendmsg");
-		return -1;
-	}
-
-	return 0;
-}
-
 int waitpid_nointr(pid_t pid, int *status, int options)
 {
 	assert(pid >= 0);
