55fa25ed71ca5bd5dd3e049369c109bad7a07fa9
Alï Polatel <alip@exherbo.org>
2021-06-16 18:42:17 +0200

    misc: add dev script to test with code coverage
    Signed-off-by: Alï Polatel <alip@exherbo.org>

---
+++ misc/conf-gcov.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -x
+
+root=$(git rev-parse --show-toplevel)
+
+. "$root"/misc/prep-env.sh
+cd "$root"
+./autogen.sh
+exec ./configure --prefix=/usr \
+    --enable-code-coverage \
+    --enable-maintainer-mode \
+    --disable-static \
+    --disable-dependency-tracking
