1ddf0454c0756bfbf88f9e7962f030ed3660f06c
Alï Polatel <alip@exherbo.org>
2021-06-16 18:41:55 +0200

    dev: add conf-afl.sh dev script
    Signed-off-by: Alï Polatel <alip@exherbo.org>

---
+++ misc/conf-afl.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -x
+
+root=$(git rev-parse --show-toplevel)
+
+. "$root"/misc/prep-env.sh
+cd "$root"
+./autogen.sh
+exec env \
+    CC=afl-gcc CXX=afl-g++ \
+    ./configure --prefix=/usr \
+    --disable-code-coverage \
+    --enable-maintainer-mode \
+    --disable-static \
+    --disable-dependency-tracking
