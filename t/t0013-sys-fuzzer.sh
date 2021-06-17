114df2436b0ecd24880022d0c3cd0dd4e9595f38
Alï Polatel <alip@exherbo.org>
2021-06-17 16:29:31 +0000

    sydbox: initial system call fuzzing tests
    Signed-off-by: Alï Polatel <alip@exherbo.org>

---
+++ t/t0013-sys-fuzzer.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Copyright 2021 Ali Polatel <alip@exherbo.org>
+# Released under the terms of the GNU General Public License v2
+
+test_description='test fuzzing system calls under SydBox'
+. ./test-lib.sh
+
+save_SYDBOX_TEST_OPTIONS="$SYDBOX_TEST_OPTIONS"
+SYDBOX_TEST_OPTIONS=''
+export SYDBOX_TEST_OPTIONS
+
+for syscall_name in \
+        mmap{,2} mprotect ioctl lstat statx \
+        access faccessat{,2} open openat{,2} \
+        creat chmod fchmodat chown lchown \
+        fchownat mkdir{,at} mknod{,at} \
+        rmdir truncate utime utimes utimensat \
+        futimesat unlink{,at} link{,at} \
+        rename renameat{,2} symlink{,at} \
+        execve execveat \
+        bind connect sendto listen accept{,4} \
+        getsockname sendmsg recvmsg \
+        {,l}listxattr {,l}setxattr {,l}removexattr \
+        mount umount{,2}
+do
+    for memory_access in 0 1 2 3; do
+        test_expect_failure EXPENSIVE,TRINITY \
+            "fuzzing $syscall_name does not generate any failures [memory_access:$memory_access]" '
+        sydbox \
+            -M '$memory_access' \
+            -c "${TEST_DIRECTORY}/test-data/trinity.syd-2" \
+            -m "allowlist/read+${HOMER}/***" \
+            -m "allowlist/write+${HOMER}/***" \
+            -- timeout -k3 15 trinity -q --stats -l off -N64 -c '$syscall_name'
+'
+    done
+done
+
+test_done
