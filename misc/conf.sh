1d949b45fcd86a7fdf3093c5dfcdf6b0e741cfc4
Alï Polatel <alip@exherbo.org>
2021-06-29 04:42:40 +0200

    misc: update conf.sh script CFLAGS
    Signed-off-by: Alï Polatel <alip@exherbo.org>

--- misc/conf.sh
+++ misc/conf.sh
@@ -5,6 +5,8 @@ root=$(git rev-parse --show-toplevel)
 . "$root"/misc/prep-env.sh
 cd "$root"
 ./autogen.sh
+CFLAGS="-O2 -pipe -ggdb"
+export CFLAGS
 exec "$root"/./configure --prefix=/usr --sysconfdir=/etc \
     --disable-code-coverage \
     --enable-maintainer-mode \
