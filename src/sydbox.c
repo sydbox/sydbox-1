5b02a962c3904f9d3e908349c74becf3bef24826
Alï Polatel <alip@exherbo.org>
2021-06-30 21:47:37 +0000

    sydbox: avoid updating chdir after every execve
    Signed-off-by: Alï Polatel <alip@exherbo.org>

--- src/sydbox.c
+++ src/sydbox.c
@@ -1696,7 +1696,7 @@ pid_validate:
 					free(current->abspath);
 					current->abspath = NULL;
 				}
-				sysx_chdir(current);
+				//sysx_chdir(current);
 			} else {
 				;/*not_exec = false;*/
 			}
