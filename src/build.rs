2215a55c3055951e7fbab8db109644b9c4866526
Alï Polatel <alip@exherbo.org>
2021-07-02 13:37:05 +0000

    more rust build updates
    Signed-off-by: Alï Polatel <alip@exherbo.org>

--- src/build.rs
+++ src/build.rs
@@ -1,11 +1,17 @@
 //
-// pandora: Sydbox's Dump Inspector & Profile Writer
+// SydB☮x: SydB☮x' Rust API
 // build.rs: Helper file for build-time information
 //
 // Copyright (c) 2021 Ali Polatel <alip@exherbo.org>
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+extern crate cbindgen;
+
 fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
+
+    cbindgen::generate(".")
+        .expect("Unable to generate C bindings.")
+        .write_to_file("sydbox_rs.h");
 }
