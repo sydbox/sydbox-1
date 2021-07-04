cd59098dfd41f61c9dd914121a1a3bc591b7cc56
Ⓐlï P☮latel <alip@exherbo.org>
2021-07-04 04:39:11 +0200

    sydbox_rs: add mod.rs
    Signed-off-by: Ⓐlï P☮latel <alip@exherbo.org>

---
+++ src/mod.rs
@@ -0,0 +1,9 @@
+//
+// SydB☮x: SydB☮x' Rust API
+// sydbox.rs: Module definitions
+//
+// Copyright (c) 2021 Ali Polatel <alip@exherbo.org>
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+pub mod sydbox;
